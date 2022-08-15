2dca9945578ec9d68e005f535b2cd8cdd6382405
sgarcia <sgarcia@athenaworks.io>
2022-08-15 00:05:37 -0300

    add validations

--- src/books/dto/create-book.dto.ts
+++ src/books/dto/create-book.dto.ts
@@ -1,5 +1,15 @@
+import {IsString, IsNotEmpty, IsNumber} from 'class-validator';
+
 export class CreateBookDto {
+  @IsString()
+  @IsNotEmpty()
   name: string
+
+  @IsString()
+  @IsNotEmpty()
   description: string
+
+  @IsNumber()
+  @IsNotEmpty()
   authorId: number
 }
